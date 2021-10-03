ef08af7527c8a85d9ca35b9ce0f2b3ba15b61fea
ndzy01 <2301173216@qq.com>
2021-10-03 15:03:15 +0800

    3 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Button from './src/components/button';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button />
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('root'));
---
+++ src/components/button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <div>
+      <Button>111</Button>
+    </div>
+  );
+};
+
+export default App;
