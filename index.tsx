88e8e5fd74ab56544a510ee52e6a6b8592f712d7
ndzy01 <2301173216@qq.com>
2021-10-04 19:04:54 +0800

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useCallback } from 'react';
+import T1 from './src/docs/t1';
 import { createContainer } from 'unstated-next';
 import { render } from 'react-dom';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const markdown = `Just a link: https://reactjs.com.`;
 
 function useCounter(initialState = 0) {
   let [count, setCount] = useState(initialState);
@@ -29,6 +34,7 @@ function App() {
 
   return (
     <Counter.Provider>
+      <T1 />
       <CounterDisplay />
       <Counter.Provider initialState={2}>
         <div>
---
+++ src/docs/t1.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+const markdown = `
+## 插件
+
+- Auto Close Tag
+- Auto Import
+- Auto Rename Tag
+- Bracket Pair Colorizer
+- CSS Peek
+- ES7 React/Redux/GraphQL/React-Native snippets
+- ESLint
+- filesize
+- Git History
+- Git History Diff
+- GitLens — Git supercharged
+- Highlight Matching Tag
+- Markdown All in One
+- Path Intellisense
+- Prettier - Code formatter
+- TODO Highlight
+- vscode-icons
+
+## 快捷键
+
+- Cmd + G 跳转
+- Opt + 左右方向键 单词间跳转
+- Cmd + 箭头
+
+`;
+
+export default () => (
+  <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+);
